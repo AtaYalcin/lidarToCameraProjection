6bb5bb5e49d8535ad036d1da9e5719d8ed4c4e35
ata <ata.yalcin@metu.edu.tr>
2024-03-05 16:47:52 +0300

    removal of type checks

--- main.py
+++ main.py
@@ -24,22 +24,24 @@ class ProjectionTool:
         self.__cameraIntrinsicMatrix = cameraIntrinsicMatrix
     def setlistOfTransformationMatrices(self,listOfTransformationMatrices):
         self.__listOfTransformationMatrices = listOfTransformationMatrices
+    def setColorFunction(self,color_function):
+        self.__color_function = color_function
 
-    def __init__(self,cameraIntrinsicMatrix,listOfTransformationMatrices):
+    def __init__(self,cameraIntrinsicMatrix,listOfTransformationMatrices,color_function=""):
         self.setCameraIntrinsic(cameraIntrinsicMatrix)
         self.setlistOfTransformationMatrices(listOfTransformationMatrices)
-        self.__rainbow_r = cm.get_cmap('rainbow_r', lut=100)
-        self.__get_color = lambda z: [val for val in self.__rainbow_r(int(z.round()))[:3]]
+        if color_function == "":
+            rainbow_r = cm.get_cmap('rainbow_r', lut=100)
+            self.__color_function = lambda z: [val for val in rainbow_r(int(z.round()))[:3]]
+        else:
+            self.__color_function = color_function
 
-    def applyLidarToCameraProjections(self,inputDirectory,outputDirectory,imageshape=(376, 1241),remove_plane = True,remove_outliers=True,color_map = "",cameraDirection = CameraDirection.POSITIVE):
-        if color_map == "":
-            color_map = self.__get_color
-        image = np.zeros((imageshape[0], imageshape[1], 3))
-
+    def applyLidarToCameraProjections(self,inputDirectory,outputDirectory,imageshape=(376, 1241),remove_plane = True,remove_outliers=True,cameraDirection = CameraDirection.POSITIVE):
         operand = self.__computeTotalMatrix()
         paths = sorted(glob(os.path.join(inputDirectory, '*.bin')))
 
         for index in range(len(paths)):
+            image = np.zeros((imageshape[0], imageshape[1], 3))
             inputPath = paths[index]
             scan_data = np.fromfile(inputPath, dtype=np.float32).reshape((-1, 4))
             xyz = scan_data[:, 0:3]
@@ -74,7 +76,7 @@ class ProjectionTool:
             # get camera coordinates u,v,z
             camera[:2] /= camera[2, :]
 
-            # remove outliers (points outside of the image frame)
+            # remove outliers (points outside the image frame)
             if remove_outliers:
                 u, v, z = camera
                 img_h, img_w, _ = image.shape
@@ -88,7 +90,7 @@ class ProjectionTool:
             u, v, z = uvz
             # draw LiDAR point cloud on blank image
             for i in range(len(u)):
-                cv2.circle(image, (int(u[i]), int(v[i])), 1,color_map(z[i]), -1)
+                cv2.circle(image, (int(u[i]), int(v[i])), 1,self.__color_function(z[i]), -1)
             matplotlib.image.imsave("{}/{}.png".format(outputDirectory,inputPath.split("/")[-1]),image)
 
 
